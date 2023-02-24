551a93e1b537cd2177c31f42c72b24f7343f3f3f
Кондратьев Алексей Александрович <AKondratyev3@alfabank.ru>
2023-02-24 19:04:14 +0300

    module 2 task 3

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -1,7 +1,12 @@
 package course.concurrency.m2_async.cf.min_price;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class PriceAggregator {
 
@@ -18,7 +23,26 @@ public class PriceAggregator {
     }
 
     public double getMinPrice(long itemId) {
-        // здесь будет ваш код
-        return 0;
+
+        List<CompletableFuture<Double>> completableFutures = new ArrayList<>();
+
+        for (var shopId : shopIds) {
+            completableFutures.add(
+                    CompletableFuture
+                            .supplyAsync(() -> priceRetriever.getPrice(itemId, shopId))
+                            .orTimeout(2, TimeUnit.SECONDS) // Поставил 2 чтобы общее время было меньше 3
+                            .handle((price, ex) -> ex != null ? Double.NaN : price)
+            );
+        }
+
+        CompletableFuture
+                .allOf(completableFutures.toArray(new CompletableFuture[shopIds.size()]))
+                .join();
+
+        return completableFutures.stream()
+                .filter(CompletableFuture::isDone)
+                .map(CompletableFuture::join)
+                .min(Comparator.naturalOrder())
+                .orElse(Double.NaN);
     }
 }
