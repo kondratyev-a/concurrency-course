82ae946ff73dcfa6453fb4798cedd25d13fdcef6
Кондратьев Алексей Александрович <AKondratyev3@alfabank.ru>
2023-03-22 12:06:22 +0300

    module 3 collections

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -1,9 +1,9 @@
 package course.concurrency.m3_shared.collections;
 
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class RestaurantService {
 
@@ -13,7 +13,7 @@ public class RestaurantService {
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private ConcurrentHashMap<String, Integer> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +21,12 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // your code
+        stat.merge(restaurantName, 1, Integer::sum);
     }
 
     public Set<String> printStat() {
-        // your code
-        return new HashSet<>();
+        return stat.keySet().stream()
+                .map(key -> String.format("%s - %s", key, stat.get(key)))
+                .collect(Collectors.toSet());
     }
 }
